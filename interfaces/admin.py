ad29eeda603bc04631c19fa23b02a587dd828fc5
Ryszard Napierała <cozajeden@gmail.com>
2021-12-27 19:16:00 +0100

    after flush. statuses won't init

--- interfaces/admin.py
+++ interfaces/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Interface, Order, Status
+from .models import Interface, Order, Status, Command
 
 # Register your models here.
 admin.site.register(Interface)
 admin.site.register(Order)
 admin.site.register(Status)
+admin.site.register(Command)
