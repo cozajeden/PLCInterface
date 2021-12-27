94ca354b9d8685cdaee63c5b925e9eff6a0014b7
Ryszard Napierała <cozajeden@gmail.com>
2021-12-27 20:24:10 +0100

    JS now which button is clicked, logging it to console when clicked

--- interfaces/consumers.py
+++ interfaces/consumers.py
@@ -12,18 +12,9 @@ class PLCInterfaceConsumer(AsyncConsumer):
             "type": "websocket.accept"
         })
         interface_name = self.scope['url_route']['kwargs']['interface_name']
-        print(self.scope['user'])
-        await asyncio.sleep(10)
-        await self.send({
-            "type": "websocket.close",
-        })
 
     async def websocket_receive(self, event):
         print("received", event)
-        await self.send({
-            "type": "websocket.send",
-            "text": f"Hello {event['text']}!"
-        })
 
     async def websocket_disconnect(self, event):
         print("disconnected", event)
