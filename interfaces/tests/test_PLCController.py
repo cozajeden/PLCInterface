1c401af28177dd0ab3bd13c317bd6c84a519c4a9
Ryszard Napierała <cozajeden@gmail.com>
2021-12-31 17:59:37 +0100

    Refactored PLCController tests

--- interfaces/tests/test_PLCController.py
+++ interfaces/tests/test_PLCController.py
@@ -4,7 +4,7 @@ import dummyPLC
 import threading
 import time
 
-class PLCControllerTest(TestCase):
+class PLCControllerTestCase(TestCase):
     def setUp(self):
         self.dummmy_plc_stop_event = threading.Event()
         dummy_plc_started = threading.Event()
@@ -23,57 +23,48 @@ class PLCControllerTest(TestCase):
         time.sleep(2)
 
     def test_can_send_read_order_command(self):
-        command = bytearray.fromhex('00000006010300ff0001')
-        success, order_number, msg, recv = self.plc.send(command)
+        success, order_number, msg, recv = self.plc.read_order_from_PLC('interface1')
         self.assertTrue(success)
         self.assertEqual(order_number, 0)
         self.assertEqual(recv, '0001000000050103020000')
 
     def test_can_send_read_amount_command(self):
-        command = bytearray.fromhex('00000006010300000001')
-        success, amount, msg, recv = self.plc.send(command)
+        success, amount, msg, recv = self.plc.read_amount_from_PLC('interface1')
         self.assertTrue(success)
         self.assertEqual(amount, 0)
         self.assertEqual(recv, '0001000000050103020000')
 
     def test_can_send_read_status_command(self):
-        command = bytearray.fromhex('00000006010100100001')
-        success, status, msg, recv = self.plc.send(command)
+        success, status, msg, recv = self.plc.read_status_from_PLC('interface1')
         self.assertTrue(success)
         self.assertEqual(status, 0)
         self.assertEqual(recv, '00010000000401010100')
 
     def test_can_send_write_read_amount_command(self):
-        command = bytearray.fromhex('00000009011000000001020019')
-        success, value, msg, recv = self.plc.send(command)
+        success, value, msg, recv = self.plc.write_amount_to_PLC('interface1', 25)
         self.assertTrue(success)
         self.assertEqual(recv, '000100000006011000000001')
 
-        command = bytearray.fromhex('00000006010300000001')
-        success, amount, msg, recv = self.plc.send(command)
+        success, amount, msg, recv = self.plc.read_amount_from_PLC('interface1')
         self.assertTrue(success)
         self.assertEqual(amount, 25)
         self.assertEqual(recv, '0002000000050103020019')
 
     def test_can_send_write_read_order_command(self):
-        command = bytearray.fromhex('00000009011000ff0001020019')
-        success, value, msg, recv = self.plc.send(command)
+        success, value, msg, recv = self.plc.write_order_to_PLC('interface1',24)
         self.assertTrue(success)
         self.assertEqual(recv, '000100000006011000ff0001')
 
-        command = bytearray.fromhex('00000006010300ff0001')
-        success, order_number, msg, recv = self.plc.send(command)
+        success, order_number, msg, recv = self.plc.read_order_from_PLC('interface1')
         self.assertTrue(success)
-        self.assertEqual(order_number, 25)
-        self.assertEqual(recv, '0002000000050103020019')
+        self.assertEqual(order_number, 24)
+        self.assertEqual(recv, '0002000000050103020018')
 
     def test_can_send_start_stop_command(self):
-        command = bytearray.fromhex('0000000601050010ff00')
-        success, value, msg, recv = self.plc.send(command)
+        success, value, msg, recv = self.plc.write_status_to_PLC('interface1', True)
         self.assertTrue(success)
         self.assertEqual(recv, '00010000000601050010ff00')
 
-        command = bytearray.fromhex('00000006010500100000')
-        success, value, msg, recv = self.plc.send(command)
+        success, value, msg, recv = self.plc.write_status_to_PLC('interface1', False)
         self.assertTrue(success)
         self.assertEqual(recv, '000200000006010500100000')
