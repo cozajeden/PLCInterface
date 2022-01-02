f01d9a0f3c35a5520fe315764f37ca31b69062c6
Ryszard Napierała <cozajeden@gmail.com>
2022-01-02 17:01:52 +0100

    deleted unused function

--- interfaces/views.py
+++ interfaces/views.py
@@ -24,24 +24,6 @@ class InterfaceView(TemplateView):
     template_name = 'interfaces/interface.html'
     model = models.Order
     form_class = forms.OrderForm
-    
-    def post(self, request, *args, **kwargs):
-        """
-        Method for processing POST request
-        """
-        print(kwargs)
-        form = forms.OrderForm(request.POST)
-        return render(
-            request,
-            self.template_name,
-            {
-                'form': form,
-                'table_fields': TABLE_HEADERS,
-                'status': NO_CONNECTED_PLC_ERROR,
-                'order': 0,
-                'amount': 0
-            }
-        )
 
     def get_context_data(self, **kwargs) -> Dict[str, Any]:
         """
